6edaaca5823e09b865eb5b76016a2e74494e8ed1
Alex K <a.katrynets@gmail.com>
2020-10-01 22:35:30 +0300

    add unprojected version of repository count

--- src/BetterExtensions.Domain/Repository/IReadRepository.cs
+++ src/BetterExtensions.Domain/Repository/IReadRepository.cs
@@ -19,6 +19,10 @@ namespace BetterExtensions.Domain.Repository
             ProjectedQueryParams<TView, TResult> projectedQueryParams,
             CancellationToken cancellationToken = default);
 
+        Task<int> CountAsync(
+            Expression<Func<TView, bool>> wherePredicate,
+            CancellationToken cancellationToken = default);
+
         Task<int> CountAsync<TResult>(
             Expression<Func<TView, TResult>> projection, 
             Expression<Func<TResult, bool>> whereProjectedPredicate,
